b3d48d552449cdc37f63447d5f21c5a0a3d60d00
ilja21267-tech <ilja21267@gmail.com>
2025-12-15 15:49:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const videos = document.getElementById("videos");
 
-const data = Array.from({ length: 12 }).map((_, i) => ({
-  title: `Видео ${i + 1}`,
+const data = Array.from({ length: 20 }).map((_, i) => ({
+  title: `Очень интересное видео ${i + 1}`,
   channel: `Канал ${i + 1}`,
-  views: `${(i + 1) * 12} тыс. просмотров`,
-  thumb: `https://picsum.photos/seed/${i}/480/270`
+  views: `${(i + 1) * 15} тыс. просмотров`,
+  time: `${i + 1} день назад`,
+  thumb: `https://picsum.photos/seed/youtube${i}/480/270`
 }));
 
 data.forEach(v => {
@@ -12,13 +13,13 @@ data.forEach(v => {
   card.className = "card";
 
   card.innerHTML = `
-    <img class="thumb" src="${v.thumb}">
+    <img class="thumbnail" src="${v.thumb}">
     <div class="info">
       <div class="channel"></div>
       <div class="text">
         <h4>${v.title}</h4>
         <p>${v.channel}</p>
-        <p>${v.views}</p>
+        <p>${v.views} • ${v.time}</p>
       </div>
     </div>
   `;
