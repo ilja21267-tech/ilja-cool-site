2f13d17f59bcb35da1ec391f40997555d5f19b11
ilja21267-tech <ilja21267@gmail.com>
2025-12-15 15:54:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
-const videos = document.getElementById("videos");
-
-const data = Array.from({ length: 20 }).map((_, i) => ({
-  title: `Очень интересное видео ${i + 1}`,
-  channel: `Канал ${i + 1}`,
-  views: `${(i + 1) * 15} тыс. просмотров`,
-  time: `${i + 1} день назад`,
-  thumb: `https://picsum.photos/seed/youtube${i}/480/270`
-}));
-
-data.forEach(v => {
-  const card = document.createElement("div");
-  card.className = "card";
-
-  card.innerHTML = `
-    <img class="thumbnail" src="${v.thumb}">
-    <div class="info">
-      <div class="channel"></div>
-      <div class="text">
-        <h4>${v.title}</h4>
-        <p>${v.channel}</p>
-        <p>${v.views} • ${v.time}</p>
-      </div>
-    </div>
-  `;
-
-  videos.appendChild(card);
-});
-
+function goShop() {
+  alert("Переход в магазин 🚀");
+  // сюда можно вставить ссылку:
+  // window.location.href = "https://твой-магазин.de";
+}
