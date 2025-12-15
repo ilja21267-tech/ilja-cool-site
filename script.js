293de1701c65e4d23e71bf3e1d416d40a8850d10
ilja21267-tech <ilja21267@gmail.com>
2025-12-15 16:04:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-function scrollToSection() {
-  document.getElementById("about").scrollIntoView({
+function goTo(id) {
+  document.getElementById(id).scrollIntoView({
     behavior: "smooth"
   });
 }
 
-function sayHello() {
-  alert("Привет! 👋 Спасибо, что зашёл на сайт.");
+function changeText() {
+  const title = document.getElementById("title");
+  title.textContent = "Кнопки реально работают 🚀";
 }
 
+function showProject(num) {
+  alert("Ты открыл проект №" + num);
+}
+
+function sayHi() {
+  alert("Спасибо! Сообщение отправлено 👍 (почти 😄)");
+}
